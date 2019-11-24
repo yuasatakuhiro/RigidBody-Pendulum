e81f1fccaf7f2019c4ec66ca38c0e53c1109ec9f
yuasatakuhiro <s1910881@s.tsukuba.ac.jp>
2019-11-24 21:22:41 +0900

    made simulator, but occur bug
    48 line

--- main/component/src/Main.cpp
+++ main/component/src/Main.cpp
@@ -1,222 +1,58 @@
 #include <Siv3D.hpp>
-// シーンの名前
-enum class State
-{
-    Title,
-    Game
-};
+#include <cmath>
 
-// ゲームデータ
-struct GameData
-{
-    // ハイスコア
-    int32 highScore = 0;
-};
-
-// シーン管理クラス
-using MyApp = SceneManager<State, GameData>;
-
-// タイトルシーン
-class Title : public MyApp::Scene
-{
-private:
-
-    Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
-    Transition m_startTransition = Transition(0.4s, 0.2s);
-
-    Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
-    Transition m_exitTransition = Transition(0.4s, 0.2s);
-
-public:
-
-    Title(const InitData& init)
-        : IScene(init) {}
-
-    void update() override
-    {
-        m_startTransition.update(m_startButton.mouseOver());
-        m_exitTransition.update(m_exitButton.mouseOver());
-
-        if (m_startButton.mouseOver() || m_exitButton.mouseOver())
-        {
-            Cursor::RequestStyle(CursorStyle::Hand);
-        }
-
-        if (m_startButton.leftClicked())
-        {
-            changeScene(State::Game);
-        }
-
-        if (m_exitButton.leftClicked())
-        {
-            System::Exit();
-        }
-    }
-
-    void draw() const override
-    {
-        const String titleText = U"ブロックくずし";
-        const Vec2 center(Scene::Center().x, 120);
-        FontAsset(U"Title")(titleText).drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
-        FontAsset(U"Title")(titleText).drawAt(center);
-
-        m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
-        m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
-
-        FontAsset(U"Menu")(U"はじめる").drawAt(m_startButton.center(), ColorF(0.25));
-        FontAsset(U"Menu")(U"おわる").drawAt(m_exitButton.center(), ColorF(0.25));
-
-        Rect(0, 500, Scene::Width(), Scene::Height() - 500)
-            .draw(Arg::top = ColorF(0.0, 0.0), Arg::bottom = ColorF(0.0, 0.5));
-
-        const int32 highScore = getData().highScore;
-        FontAsset(U"Score")(U"High score: {}"_fmt(highScore)).drawAt(Vec2(620, 550));
-    }
-};
-
-// ゲームシーン
-class Game : public MyApp::Scene
-{
-private:
-
-    // ブロックのサイズ
-    static constexpr Size blockSize = Size(40, 20);
-
-    // ボールの速さ
-    static constexpr double speed = 480.0;
-
-    // ブロックの配列
-    Array<Rect> m_blocks;
-
-    // ボールの速度
-    Vec2 m_ballVelocity = Vec2(0, -speed);
-
-    // ボール
-    Circle m_ball = Circle(400, 400, 8);
-
-    // パドル
-    Rect m_paddle = Rect(Arg::center(Cursor::Pos().x, 500), 60, 10);
-
-    // スコア
-    int32 m_score = 0;
-
-public:
-
-    Game(const InitData& init)
-        : IScene(init)
-    {
-        // 横 (Scene::Width() / blockSize.x) 個、縦 5 個のブロックを配列に追加する
-        for (auto p : step(Size((Scene::Width() / blockSize.x), 5)))
-        {
-            m_blocks << Rect(p.x * blockSize.x, 60 + p.y * blockSize.y, blockSize);
-        }
-
-    }
-
-    void update() override
-    {
-        // パドルを操作
-        m_paddle = Rect(Arg::center(Cursor::Pos().x, 500), 60, 10);
-
-        // ボールを移動
-        m_ball.moveBy(m_ballVelocity * Scene::DeltaTime());
-
-        // ブロックを順にチェック
-        for (auto it = m_blocks.begin(); it != m_blocks.end(); ++it)
-        {
-            // ボールとブロックが交差していたら
-            if (it->intersects(m_ball))
-            {
-                // ボールの向きを反転する
-                (it->bottom().intersects(m_ball) || it->top().intersects(m_ball) ? m_ballVelocity.y : m_ballVelocity.x) *= -1;
-
-                // ブロックを配列から削除（イテレータが無効になるので注意）
-                m_blocks.erase(it);
-
-                // スコアを加算
-                ++m_score;
-
-                // これ以上チェックしない
-                break;
-            }
-        }
-
-        // 天井にぶつかったらはね返る
-        if (m_ball.y < 0 && m_ballVelocity.y < 0)
-        {
-            m_ballVelocity.y *= -1;
-        }
-
-        if (m_ball.y > Scene::Height())
-        {
-            changeScene(State::Title);
-            getData().highScore = Max(getData().highScore, m_score);
-        }
-
-        // 左右の壁にぶつかったらはね返る
-        if ((m_ball.x < 0 && m_ballVelocity.x < 0) || (Scene::Width() < m_ball.x && m_ballVelocity.x > 0))
-        {
-            m_ballVelocity.x *= -1;
-        }
-
-        // パドルにあたったらはね返る
-        if (m_ballVelocity.y > 0 && m_paddle.intersects(m_ball))
-        {
-            // パドルの中心からの距離に応じてはね返る向きを変える
-            m_ballVelocity = Vec2((m_ball.x - m_paddle.center().x) * 10, -m_ballVelocity.y).setLength(speed);
-        }
-    }
-
-    void draw() const override
-    {
-        FontAsset(U"Score")(m_score).drawAt(Scene::Center().x, 30);
-
-        // すべてのブロックを描画する
-        for (const auto& block : m_blocks)
-        {
-            block.stretched(-1).draw(HSV(block.y - 40));
-        }
-
-        // ボールを描く
-        m_ball.draw();
-
-        // パドルを描く
-        m_paddle.draw();
-    }
+namespace Initial {
+double theta = 60_deg;
+double theta_vel = 0.3;
+float Mass = 60;
+float length = 2*100;
+float width = 30;
+float bind_length = length/2.0;
+double Iz = Mass*(length/2.0)*(length/2.0)/3.0+Mass*bind_length*bind_length;
+float g = 980;
 };
 
 void Main()
 {
-    // 使用するフォントアセットを登録
-    FontAsset::Register(U"Title", 120, U"example/font/AnnyantRoman/AnnyantRoman.ttf");
-    FontAsset::Register(U"Menu", 30, Typeface::Regular);
-    FontAsset::Register(U"Score", 36, Typeface::Bold);
+    Scene::SetBackground(Palette::Skyblue);
     
-    Window::SetStyle(WindowStyle::Sizable);
-    //Scene::SetScaleMode(ScaleMode::ResizeFill);
-    Window::Maximize();
-    Scene::SetLetterbox(Palette::Black);
-    Window::SetStyle(WindowStyle::Sizable);
-    //Scene::SetScaleMode(ScaleMode::ResizeFill);
-    Window::Maximize();
-    Scene::SetLetterbox(Palette::Black);
-
-    // 背景色を設定
-    Scene::SetBackground(ColorF(0.2, 0.8, 0.4));
-
-    // シーンと遷移時の色を設定
-    MyApp manager;
-    manager
-        .add<Title>(State::Title)
-        .add<Game>(State::Game)
-        .setFadeColor(ColorF(1.0));
+    const Rect stick(0,0,Initial::width,Initial::length);
+    const Vec2 bind_point(stick.pos.x+Initial::width/2.0,stick.pos.y);
     
-
-    while (System::Update())
-    {
-        if (!manager.update())
-        {
-            break;
+    float theta_vel = 0;
+    float theta = 80_deg;
+    
+    while(System::Update()){
+        const Transformer2D t0(Mat3x2::Translate(Scene::Center()-bind_point),true);
+        const float theta_f = theta;
+        const float theta_ff = theta_vel;
+        
+        theta_vel = theta_vel - Scene::DeltaTime()*Initial::g*sin(theta)/Scene::Center().y;
+        theta = theta + Scene::DeltaTime()*theta_vel;
+        
+        const Vec2 pos = OffsetCircular(Vec2(bind_point.x,bind_point.y-Scene::Center().y),static_cast<double>(Scene::Center().y),180_deg+theta);
+        
+        const Vec2 pos_1 = OffsetCircular(Vec2(bind_point.x,bind_point.y-Scene::Center().y),static_cast<double>(Scene::Center().y),180_deg+theta_f);
+        
+        const Vec2 pos_2 = OffsetCircular(Vec2(bind_point.x,bind_point.y-Scene::Center().y),static_cast<double>(Scene::Center().y),180_deg+theta+(theta-theta_f));
+        
+        Vec2 pos_delta = (pos_2-pos_1);
+        if(pos_delta.length() == 0){
+            pos_delta = Vec2(0,0);
+        }else{
+            pos_delta = pos_delta/pos_delta.length();
         }
+        const Vec2 theta_a = abs((theta_vel - theta_ff)/Scene::DeltaTime())*pos_delta;
+        
+        
+        Initial::theta_vel = Initial::theta_vel -  Scene::DeltaTime()*(Initial::Mass*Initial::g*Initial::length/2.0*sin(Initial::theta)/Initial::Iz) + (Initial::length/2.0)*cos(Initial::theta)*theta_a.y/Initial::Iz-(Initial::length/2.0)*sin(Initial::theta)*theta_a.x/Initial::Iz;
+        
+        Initial::theta = Initial::theta + Scene::DeltaTime()*Initial::theta_vel;
+        
+        Circle(bind_point.movedBy(pos),10).draw(Palette::White);
+        Line(bind_point.movedBy(pos),Vec2(bind_point.x,-Scene::Center().y)).draw(Palette::Green);
+        
+        const Transformer2D t1(Mat3x2::Translate(pos));
+        stick.rotatedAt(bind_point,Initial::theta).draw(Palette::Black);
     }
 }
