bff5afa0b16b339907584740a7a88f958f78b6b1
yuasatakuhiro <s1910881@s.tsukuba.ac.jp>
2019-11-28 17:33:57 +0900

    add kaos

--- main/component/src/Main.cpp
+++ main/component/src/Main.cpp
@@ -1,39 +1,41 @@
 #include <Siv3D.hpp>
+#include <vector>
 #include <cmath>
 
+const int num = 10;
 namespace Initial {
-double theta1 = 90_deg;
-double theta2 = 90_deg;
-double theta_vel1 = 0.0;
-double theta_vel2 = 0.0;
-float Mass1 = 0.40;
-float Mass2 = 0.54;
-float length1 = 0.43;
-float length1_ = length1*1000;
-float length2 = 0.33;
-float length2_ = length2*1000;
-float width = 15;
+std::vector<double> theta1 = {90_deg,90.001_deg,90.002_deg,90.003_deg,90.004_deg,90.005_deg,90.006_deg,90.007_deg,90.008_deg,90.009_deg};
+std::vector<double> theta2(num,90_deg);
+std::vector<double> theta_vel1(num,0.0);
+std::vector<double> theta_vel2(num,0.0);
+std::vector<float> Mass1(num,0.40);
+std::vector<float> Mass2(num,1.0);
+std::vector<float> length1(num,0.23);
+std::vector<float> length1_(num,length1[0]*1000);
+std::vector<float> length2(num,0.13);
+std::vector<float> length2_(num,length2[0]*1000);
+std::vector<float> width(num,15);
 float g = 9.8;
 };
 
 namespace Mat_elem {
 using namespace Initial;
-double A = (Mass1*4*Mass2)*length1;
-double B = 2*Mass2*length2*cos(theta1-theta2);
-double C = 2*length1*cos(theta1-theta2);
-double D = length2;
-double D1 = -2*Mass2*length2*theta_vel2*theta_vel2*sin(theta1-theta2)-(Mass1+2*Mass2)*g*sin(theta1);
-double D2 = 2*length1*theta_vel1*theta_vel1*sin(theta1-theta2)-g*sin(theta2);
+double A[num];
+double B[num];
+double C[num];
+double D[num];
+double D1[num];
+double D2[num];
 }
 
-namespace Mat_elem2 {
+/*namespace Mat_elem2 {
 using namespace Initial;
 double A1 = (Mass1+Mass2)*length1*length1;
 double A2 = Mass2*length2*length2;
 double B = Mass2*length1*length2*cos(theta1-theta2);
 double D1 = -Mass2*length1*length2*theta_vel2*theta_vel2*sin(theta1-theta2)-(Mass1*Mass2)*g*length1*sin(theta1);
 double D2 = Mass2*length1*length2*theta_vel1*theta_vel1*sin(theta1-theta2)-Mass2*g*length2*sin(theta2);
-}
+}*/
 
 using namespace Initial;
 using namespace Mat_elem;
@@ -41,11 +43,12 @@ using namespace Mat_elem;
 void Main()
 {
     Scene::SetBackground(Palette::Skyblue);
-    const Rect stick1(0,0,Initial::width,Initial::length1_);
-    const Circle knot(0,0,10);
-    const int a = knot.center.x;
-    const int b = knot.center.y;
-    const Rect stick2(Arg::topCenter(a,b),Initial::width,Initial::length2_);
+    RectF stick1[num]; Circle knot[num]; RectF stick2[num];
+    for(auto n:step(num)){
+        stick1[n] = RectF(0,0,Initial::width[n],Initial::length1_[n]);
+        knot[n] = Circle(0,0,10);
+        stick2[n] = RectF(Arg::topCenter(knot[n].center.x,knot[n].center.y),Initial::width[n],Initial::length2_[n]);
+    }
     
     Window::SetStyle(WindowStyle::Sizable);
     Scene::SetScaleMode(ScaleMode::ResizeFill);
@@ -59,30 +62,35 @@ void Main()
     while(System::Update()){
         double t = 0.001;
         
-        for(int i = 0;i<30;i++){
-        A = (Mass1*4*Mass2)*length1;
-        B = 2*Mass2*length2*cos(theta1-theta2);
-        C = 2*length1*cos(theta1-theta2);
-        D = length2;
-        Mat_elem::D1 = -2*Mass2*length2*theta_vel2*theta_vel2*sin(theta1-theta2)-(Mass1+2*Mass2)*g*sin(theta1);
-        Mat_elem::D2 = 2*length1*theta_vel1*theta_vel1*sin(theta1-theta2)-g*sin(theta2);
+        for(auto _:step(30)){
+            for (auto j:step(num)){
+        A[j] = (Mass1[j]*4*Mass2[j])*length1[j];
+        B[j] = 2*Mass2[j]*length2[j]*cos(theta1[j]-theta2[j]);
+        C[j] = 2*length1[j]*cos(theta1[j]-theta2[j]);
+        D[j] = length2[j];
+        Mat_elem::D1[j] = -2*Mass2[j]*length2[j]*theta_vel2[j]*theta_vel2[j]*sin(theta1[j]-theta2[j])-(Mass1[j]+2*Mass2[j])*g*sin(theta1[j]);
+        Mat_elem::D2[j] = 2*length1[j]*theta_vel1[j]*theta_vel1[j]*sin(theta1[j]-theta2[j])-g*sin(theta2[j]);
+            
+        double ADBC = 1.0/A[j]*D[j]-B[j]*C[j];
         
-        double ADBC = 1.0/A*D-B*C;
+        theta_vel1[j] = theta_vel1[j] + t*(ADBC*(D[j]*Mat_elem::D1[j]-B[j]*Mat_elem::D2[j]));
         
-        theta_vel1 = theta_vel1 + t*(ADBC*(D*Mat_elem::D1-B*Mat_elem::D2));
+        theta_vel2[j] = theta_vel2[j] + t*(ADBC*(-A[j]*Mat_elem::D1[j]+C[j]*Mat_elem::D1[j]));
         
-        theta_vel2 = theta_vel2 + t*(ADBC*(-A*Mat_elem::D1+C*Mat_elem::D1));
+        theta1[j] = theta1[j] + t*theta_vel1[j];
+        theta2[j] = theta2[j] + t*theta_vel2[j];
+            }}
         
-        theta1 = theta1 + t*theta_vel1;
-        theta2 = theta2 + t*theta_vel2;
-        }
-        const Transformer2D t0(Mat3x2::Translate(Scene::Center()-Vec2(width/2.0,100)),true);
-        stick1.rotatedAt(stick1.pos+Vec2(width/2.0,0),theta1).draw(Palette::Orange);
+        for(auto i:step(num)){
+            
+        const Transformer2D t0(Mat3x2::Translate(Scene::Center()-Vec2(width[i]/2.0,100)),true);
+        stick1[i].rotatedAt(stick1[i].pos+Vec2(width[i]/2.0,0),theta1[i]).draw(Palette::Orange);
 
-        const Vec2 point = OffsetCircular(Vec2(width/2.0,0),length1_,180_deg+theta1);
+        const Vec2 point = OffsetCircular(Vec2(width[i]/2.0,0),length1_[i],180_deg+theta1[i]);
         const Transformer2D t1(Mat3x2::Translate(point));
         
-        knot.draw(Palette::Black);
-        stick2.rotatedAt(knot.center,theta2).draw(Palette::Green);
-    }
+        knot[i].draw(Palette::Black);
+        stick2[i].rotatedAt(knot[i].center,theta2[i]).draw(Palette::Green);
+            }
+        }
 }
